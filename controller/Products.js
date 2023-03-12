e390c014f65f9c83a0dd7c551956f3250c56f492
Qmun14 <mamunramdhan@gmail.com>
2023-03-12 10:02:47 +0700

    Menerapkan middleware verifyUser ke ProductRoute untuk menproteksi proses CRUD pada controller Products, membuat fitur endpoints getProducts, createProduct untuk masing2 user sesuai ketentuan role-nya pada saat login

--- controller/Products.js
+++ controller/Products.js
@@ -1,17 +1,58 @@
 import Product from '../models/ProductModel.js';
+import User from "../models/UserModel.js";
 
-export const getProducts = (req, res) => {
+export const getProducts = async (req, res) => {
+    try {
+        let response;
+        if (req.role === "admin") {
+            response = await Product.findAll({
+                attributes: ['uuid', 'name', 'price'],
+                include: [{
+                    model: User,                    //Jika user login sebagai admin maka user admin dapat.. 
+                    attributes: ['name', 'email']     //..melihat semua product yang diupload oleh seluruh user
+                }]
+            });
+        } else {
+            response = await Product.findAll({
+                attributes: ['uuid', 'name', 'price'],
+                where: {
+                    userId: req.userId
+                },
+                include: [{                             //Jika user login sebagai user-biasa maka user hanya.. 
+                    model: User,                        //..melihat semua product yang diupload oleh user tersebut
+                    attributes: ['name', 'email']
+                }]
+            });
+        }
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
 
 }
+
 export const getProductById = (req, res) => {
 
 }
-export const createProduct = (req, res) => {
 
+export const createProduct = async (req, res) => {
+    const { name, price } = req.body;
+    try {
+        await Product.create({
+            name: name,
+            price: price,
+            userId: req.userId
+        });
+        res.status(201).json({ message: "success" });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+    }
 }
+
 export const updateProduct = (req, res) => {
 
 }
+
 export const deleteProduct = (req, res) => {
 
 }
